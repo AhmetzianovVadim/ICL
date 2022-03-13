fda868ca24bbbab73dfaa14530fb73552efd27ae
Stepan <ahmetzianovvadim@yandex.ru>
2022-03-13 19:54:35 +0300

    ICL

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace TestApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Результат";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(187, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Проверить соединение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "IP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Имя ПК: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 60);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "IP: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 101);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Адрес сайта:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(187, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 30);
+            this.textBox1.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 241);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(528, 297);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(550, 550);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.MinimumSize = new System.Drawing.Size(550, 550);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace TestApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            programSettings(); //Метод, отвечающий за настройки программы
+            createShortcut(); //Метод, создающий ярлык
+            PCinfo(); //Метод выдающий информацию о компьютере пользователя
+        }
+        private void programSettings() 
+        {
+            this.Text = Properties.Settings.Default.FormName; //Устанавливаем имя формы, из файла конфигурации
+            if (Properties.Settings.Default.SaveLocation) //Проверяем, хочет ли пользователь сохранять позицию окна после закрытия
+            {
+                this.Location = Properties.Settings.Default.Location; //Устанавливает координаты окна при запуске программы
+                this.FormClosing += FormClosingEventHandler; //Привязываемся к обработчику закрытия формы
+                StartPosition = FormStartPosition.Manual; //Указываем, что хотим использовать свои координаты окна
+            } 
+            else
+                StartPosition = FormStartPosition.CenterScreen; //Стандартное положение окна в центре экрана
+            if (Properties.Settings.Default.SaveAddress) //Проверяем хочет ли пользователь сохранять адрес сайта
+            {
+                this.textBox1.Text = Properties.Settings.Default.DefaultAddressOfSite; //Устанавливаем адрес сайта при запуске программы
+                this.FormClosing += FormClosingEventHandler; ////Привязываемся к обработчику закрытия формы
+            }
+            else 
+            {
+                this.textBox1.Text = Properties.Settings.Default.DefaultAddressOfSite; //Устанавливаем адрес по умолчанию при запуске программы
+            }
+            try { this.Icon = new Icon(AppDomain.CurrentDomain.BaseDirectory + Properties.Settings.Default.Icon); } //Устанавливаем иконку формы
+            catch { MessageBox.Show("Проверьте настройки иконки в файле конфигурации\nПрограмма всё ещё способна выполнять свои функции"); }
+        }
+        public void createShortcut() 
+        {
+            WshShell shell = new WshShell(); //Создаём объект WshShell
+            string shortcutPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Ахметзянов_В_А_4310.lnk"; //указываем путь к ярылку и его название
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath); //Создаем объект ярлыка
+            shortcut.IconLocation = AppDomain.CurrentDomain.BaseDirectory + @"\" + Properties.Settings.Default.Icon; //устанавливаем иконку ярлыка
+            shortcut.Description = "Ярлык Ахметзянов Вадим Альбертович 4310"; //Добавляем описание ярлыка в всплывающей подсказке
+            shortcut.TargetPath = AppDomain.CurrentDomain.BaseDirectory + @"\TestApp2.exe"; //Указываем путь к программе
+            shortcut.Save(); //создаём ярлык
+        }
+
+        public void PCinfo() 
+        {
+            string Host = Dns.GetHostName(); // Получем имя ПК
+            label2.Text = Host; //Отображаем его на форме
+            IPAddress[] ips; // Создаём массив для IP 
+            ips = Dns.GetHostAddresses(label2.Text); // Получаем IP с помощью имени ПК  
+            foreach (IPAddress ip in ips)//Перебираем все результаты, в данном случае, один
+            {
+                label3.Text = ip.ToString(); //Выводим IP на форму
+            }
+        }
+
+        public void ping() 
+        {
+            try
+            {
+                Ping pingSender = new Ping(); //Создаём объёкт класса Ping
+                PingReply reply = pingSender.Send(textBox1.Text); //Отправляем запрос на проверку связи
+                if (reply.Status == IPStatus.Success)  //Узнаём результат
+                {
+                    label1.ForeColor = Color.Green; //Перекрашиваем текст label1 в зелёный
+                    label1.Text = "PING OK"; //Пишем, что всё в порядке
+                    richTextBox1.ForeColor = Color.Green; //Перекрашиваем текст richTextBox1  в зелёный
+                    // ↓↓↓ Выводим текст, с результатами пинга, новая информация не будет стирать предыдущую
+                    richTextBox1.AppendText("Start log\r\n" + "Адрес:" + reply.Address.ToString() + "\r\nВремя: " + reply.RoundtripTime + "мс" + "\r\nРазмер пакета: " + reply.Buffer.Length + " байта" + "\r\nEnd Log\r\n\n"); 
+                }
+                else
+                {
+                    //Соединения нет, красим текст в красный, выдаём результат
+                    richTextBox1.ForeColor = Color.Red;
+                    richTextBox1.Text = "Соединения нет\n"; //Новая информация сотрёт старую
+                    label1.ForeColor = Color.Red;
+                    label1.Text = "PING FALSE"; 
+                }
+            }
+            catch 
+            {
+                //Обрабатываем исключения, выдаём инструкции
+                MessageBox.Show("Ошибка!\n1. Проверьте правильность ввода данных в поле \"Адрес сайта\"\n2. Проверьте подключение к Интернету");
+            }    
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Обработчик нажатия на кнопку
+        {
+            ping(); //Вызываем функцию пинга
+        }
+
+        private void FormClosingEventHandler(object sender, FormClosingEventArgs e) //Обработчик события закрытия формы
+        {
+            Properties.Settings.Default.Location = this.Location; //Присваиваем последние координаты
+            Properties.Settings.Default.DefaultAddressOfSite = this.textBox1.Text; //Присваиваем последний введёный текст 
+            Properties.Settings.Default.Save(); //Сохраняем изменения
+        }
+    }
+}
+
